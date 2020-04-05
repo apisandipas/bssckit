dbbcd1015466f45d086438ac9297680907b4d8ac
Bryan Paronto <bparonto@gmail.com>
2020-04-05 03:09:06 -0500

    finalized forms

--- src/components/Forms/Form.stories.js
+++ src/components/Forms/Form.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Col } from '../Col';
 import { Button } from '../Button';
 import { FormControl } from './FormControl';
 import { FormGroup } from './FormGroup';
@@ -72,6 +73,38 @@ export const inputTypes = () => (
   </form>
 );
 
+export const sizes = () => (
+  <form>
+    <FormControl mb2 lg type='text' placeholder='Large' />
+    <FormControl mb2 type='text' placeholder='Normal' />
+    <FormControl mb2 sm type='text' placeholder='Small' />
+  </form>
+);
+
+export const sizesOnGroups = () => (
+  <form>
+    <FormGroup row justify lg mb2>
+      <FormControl type='text' placeholder='Large' />
+    </FormGroup>
+    <FormGroup row justify mb2>
+      <FormControl type='text' placeholder='Normal' />
+    </FormGroup>
+    <FormGroup row justify sm mb2>
+      <FormControl type='text' placeholder='Small' />
+    </FormGroup>
+  </form>
+);
+
+export const readonly = () => (
+  <FormControl readonly disabled type='text' placeholder='Readonly input here...' />
+);
+
+export const readonlyPlainText = () => (
+  <form>
+    <FormControl plainText readonly disabled value='Readonly input here...' />
+  </form>
+);
+
 export const inlineForm = () => (
   <FormInline>
     <FormGroup>
@@ -113,5 +146,82 @@ export const checkboxesAndRadio = () => (
         <FormCheckInput disabled type='radio' name='exampleRadios' /> Disabled radio
       </label>
     </FormCheck>
+  </form>
+);
+
+export const inlineCheckboxesAndRadios = () => (
+  <form>
+    <FormCheck inline>
+      <label>
+        <FormCheckInput type='checkbox' /> Default checkbox
+      </label>
+    </FormCheck>
+    <FormCheck inline>
+      <label>
+        <FormCheckInput disabled type='checkbox' /> Disabled checkbox
+      </label>
+    </FormCheck>
+    <FormCheck inline>
+      <label>
+        <FormCheckInput type='radio' name='exampleRadios' /> Default radio
+      </label>
+    </FormCheck>
+    <FormCheck inline>
+      <label>
+        <FormCheckInput type='radio' name='exampleRadios' /> Default radio
+      </label>
+    </FormCheck>
+    <FormCheck inline>
+      <label>
+        <FormCheckInput disabled type='radio' name='exampleRadios' /> Disabled radio
+      </label>
+    </FormCheck>
+  </form>
+);
+
+export const formGrid = () => (
+  <form>
+    <FormGroup row>
+      <Col sm={6}>
+        <FormControl type='text' placeholder='First name' />
+      </Col>
+      <Col sm={6}>
+        <FormControl type='text' placeholder='Last name' />
+      </Col>
+    </FormGroup>
+    <FormGroup row>
+      <Col sm={8}>
+        <FormControl type='text' placeholder='City' />
+      </Col>
+      <Col sm={4}>
+        <FormGroup>
+          <FormControl select>
+            <option>Choose state...</option>
+            <option>...</option>
+          </FormControl>
+        </FormGroup>
+      </Col>
+    </FormGroup>
+  </form>
+);
+
+export const horizontalGrid = () => (
+  <form>
+    <FormGroup row justify>
+      <Col sm={2}>
+        <label>First name</label>
+      </Col>
+      <Col sm={10}>
+        <FormControl type='text' placeholder='First name' />
+      </Col>
+    </FormGroup>
+    <FormGroup row justify>
+      <Col sm={2}>
+        <label>Last name</label>
+      </Col>
+      <Col sm={10}>
+        <FormControl type='text' placeholder='Last name' />
+      </Col>
+    </FormGroup>
   </form>
 );
